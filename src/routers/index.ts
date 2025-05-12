0cf18e1c9fd1d65704fc67b467f435ee90288d43
Omar Moataz <omarmoataz@outlook.com>
2025-05-12 18:49:00 +0300

    Removed fly.toml and added vercel.json back.

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -12,7 +12,6 @@ app.use(express.json());
 
 app.use("/api/products", productsRouter);
 app.use("/api/categories", categoriesRouter);
-app.use("/", "Node js running");
 
 app.listen(PORT, () => {
   console.log("Listening on port", PORT);
